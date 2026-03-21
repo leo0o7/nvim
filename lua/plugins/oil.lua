d8505535456635e7140a0f9c9080876e8f851cdc
leo0o7 <giardinoleonardo34@gmail.com>
2026-03-21 10:42:45 +0100

    added git-signs to oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,16 +1,22 @@
 return {
-	"stevearc/oil.nvim",
-	keys = {
-		{ "<leader>e", "<cmd>Oil --float<CR>", desc = "Explorer" },
-	},
-	opts = {
-		view_options = {
-			show_hidden = true,
+	{
+		"stevearc/oil.nvim",
+		keys = {
+			{ "<leader>e", "<cmd>Oil --float<CR>", desc = "Explorer" },
 		},
-		float = {
-			padding = 5,
+		opts = {
+			view_options = {
+				show_hidden = true,
+			},
+			float = {
+				padding = 5,
+			},
 		},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
-	-- Optional dependencies
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	{
+		"benomahony/oil-git.nvim",
+		dependencies = { "stevearc/oil.nvim" },
+	},
 }
