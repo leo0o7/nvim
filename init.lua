053993f9026e5e4d73a725c51146aca0cf529c47
leo0o7 <giardinoleonardo34@gmail.com>
2026-03-21 10:43:52 +0100

    fixed clipboard issues (and removed the useless language setting that was used to set language to en_US on incorrectly set up terminals)

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
 -- force set language to english
-vim.api.nvim_exec("language en_US", true)
+-- vim.api.nvim_exec("language en_US", true)
+
+-- set OSC 52 clipboard to avoid encoding issues on mac
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = { "pbpaste" },
+		["*"] = { "pbpaste" },
+	},
+}
+
 require("config.lazy")
