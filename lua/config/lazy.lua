df4c7907124e206a9e077aff186ea2e0ff7cb37d
leo0o7 <giardinoleonardo34@gmail.com>
2026-04-19 18:47:05 +0200

    change to colorscheme to catpuccin-nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
 			"LazyVim/LazyVim",
 			import = "lazyvim.plugins",
 			opts = {
-				colorscheme = "catppuccin",
+				colorscheme = "catppuccin-nvim",
 				-- colorscheme = "solarized-osaka",
 				news = {
 					lazyvim = true,
